62287709e7239080099307491985f1c74e2aa388
Don Welch <dwelch@nait.ca>
2022-03-04 12:02:25 -0700

    Added comments

--- src/ChinookSolution/WebApp/Pages/SamplePages/AlbumsByGenreQuery.cshtml.cs
+++ src/ChinookSolution/WebApp/Pages/SamplePages/AlbumsByGenreQuery.cshtml.cs
@@ -111,5 +111,10 @@ namespace WebApp.Pages.SamplePages
             }
             return RedirectToPage(new {GenreId = GenreId }); //cause a Get request which forces OnGet execution
         }
+
+        public IActionResult OnPostNew()
+        {
+            return RedirectToPage("/SamplePages/CRUDAlbum");
+        }
     }
 }
